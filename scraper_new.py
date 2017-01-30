a4883675492db06fd53304e5234599d575d60694
Piyush Jena <Piyush_Jena@Pabitras-MacBook-Pro.local>
2017-01-30 15:21:06 +0530

    Made changes to the scraper :Piyush

--- scraper_new.py
+++ scraper_new.py
@@ -1,7 +1,9 @@
 import urllib2
 import ast
+import re
+
 url = 'http://www.unitedstateszipcodes.org/91801/'
-req = urllib2.Request('http://www.unitedstateszipcodes.org/91801/', headers={ 'User-Agent': 'Mozilla/5.0' })
+req = urllib2.Request('http://www.unitedstateszipcodes.org/92335/', headers={ 'User-Agent': 'Mozilla/5.0' })
 html = urllib2.urlopen(req).read()
 
 f1 = open('test.htm','w+')
@@ -14,15 +16,15 @@ print "ZIP Code: ",
 print url[-6:-1]
 for line in f1:
     k=k+1;
-    if k>378:
-    	if k<430:
-    		if line.find("<th>")!=-1:
-    			if line.find("</th>")!=-1:
-    				print line.strip()[4:-5],
+    if k>350:
+    	if k<450:
+    		if line.find("<th>")>-1:
+    			if line.find("</th>")>-1:
+    				print re.sub('[^A-Za-z0-9]+', '', line.strip()[4:-5]),
     				print ' ',
-    		if line.find("<td class=")!=-1:
-    			if line.find("</td>")!=-1:
-    				print line.strip()[23:-5];
+    		if line.find("<td class=")>-1:
+    			if line.find("</td>")>-1:
+    				print re.sub('[^A-Za-z0-9]+', '', line.strip()[23:-5]);
     if line.find("Worked Full-time With Earnings")>-1:
         line1 = line[15:-2]
     if line.find("$10,000-$19,999")>-1:
